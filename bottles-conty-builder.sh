0f0e21810ee61f8853c12094fecd0b124a791e15
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-12-19 22:23:45 +0100

    Update bottles-conty-builder.sh

--- bottles-conty-builder.sh
+++ bottles-conty-builder.sh
@@ -69,7 +69,9 @@ cd .. || exit 1
 
 # EXPORT THE APPDIR TO AN APPIMAGE
 VERSION=$(curl -Ls https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=bottles | grep "^pkgver=" | cut -c 8-)
-ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 1 \
+printf '#!/bin/sh\nexit 0' > ./desktop-file-validate
+chmod a+x ./appimagetool ./desktop-file-validate
+PATH="$PATH:$PWD" ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 1 \
 	-u "gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|Bottles-appimage|continuous|*x86_64.AppImage.zsync" \
 	./"$APP".AppDir ./Bottles-"$VERSION"-x86_64.AppImage
 cd .. && mv ./tmp/*.AppImage* ./ || exit 1
